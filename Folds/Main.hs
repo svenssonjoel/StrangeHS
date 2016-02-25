faa4a4179c555802d354d8815109ea42878b83e3
Joel Svensson <svenssonjoel@yahoo.se>
2016-02-25 21:02:51 +0100

    update

--- Folds/Main.hs
+++ Folds/Main.hs
@@ -24,12 +24,49 @@ instance Num Expr where
   signum = undefined 
   fromInteger n = Expr (I (fromInteger n))
 
-test :: Expr
-test = 1 + 2 + 3 + 4 + 5 
 
 adds :: Expr -> Int
 adds (Expr e) = foldl isAdd 0 e
   where isAdd sum (Expr (Add _ _)) = sum + 1
+        isAdd sum _ = sum 
+
+
+
+test :: Expr
+test = 1 + 2 + 3 + 4 + 5
+
+test2 :: Expr
+test2 = 1 * 2 + 3 - 1 + 2 + 3 + 4 - 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+foldExp :: (a -> Exp Expr -> a) -> a -> Exp Expr ->  a
+foldExp f a e@(I i) = f a e
+foldExp f a e@(Add (Expr e1) (Expr e2)) = f (foldExp f (foldExp f a e2) e1) e
+foldExp f a e@(Sub (Expr e1) (Expr e2)) = f (foldExp f (foldExp f a e2) e1) e
+foldExp f a e@(Mul (Expr e1) (Expr e2)) = f (foldExp f (foldExp f a e2) e1) e
+
+
+adds_ :: Expr -> Int
+adds_ (Expr e) = foldExp isAdd 0 e
+  where isAdd sum (Add _ _) = sum + 1
         isAdd sum _ = sum 
 
 
